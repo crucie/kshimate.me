2b37307e298ff9ef65ae84cd5a31e3c48e7c7a56
crucie <amaymishra100@gmail.com>
2025-05-09 23:21:38 +0530

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import { FloatingDock } from './Components/ui/FloatingDock'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-
-
 
 function App() {
-
-  const items = [
-    { title: 'Home', icon: <HomeIcon />, href: '/' },
-    { title: 'About', icon: <AboutIcon />, href: '/about' },
-    // Add more items as needed
-  ];
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Router>
-      <FloatingDock items={items} desktopClassName="fixed bottom-0 " mobileClassName="fixed bottom-0 right-0" />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        {/* Add other routes here */}
-      </Routes>
-    </Router>
+    <div className='w-full justify-center items-center flex '>
+      HELLO WORLD!
+    </div>
     </>
   )
 }
-
-
-const HomeIcon = () => <div>🏠</div>;
-const AboutIcon = () => <div>ℹ️</div>;
-
-const Home = () => <div>Home Page</div>;
-const About = () => <div>About Page</div>;
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import Home from './Components/Home.jsx'
+import App from './App.jsx'
 
-const route = createBrowserRouter(
-  createRoutesFromElements([
-    <Route path="/" element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-    </Route>
-  ])
-)
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <RouterProvider router = {route}/> */}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
