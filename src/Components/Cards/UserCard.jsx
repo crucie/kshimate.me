b39127b18ec1b8b2491d78a436f6ae5acfbab570
crucie <amaymishra100@gmail.com>
2025-05-11 03:06:38 +0530

    minor work on userCard

--- src/Components/Cards/UserCard.jsx
+++ src/Components/Cards/UserCard.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cardData from '../../Data/CardData.json'
 
-function UserCard() {
+const UserCard = (cardData) => {
+
+  cosnt {id, title} = cardData
+
+
   return (
-    <div>
-        
+    <div className=' flex flex-col p-4 items-center w-[300px] rounded-xl h-full aspect-[3/4] border-1 shadow-md transition-500 bg-gray-50 hover:scale-102 m-4'>
+        <div className='h-2/3 w-full bg-gray-50 border-2 border-dashed my-2'>
+
+        </div>
+        <div className='w-full  flex flex-col items-start justify-items-start p-2 my-2'>
+          <h3 className='font-bold text-2xl font-sans'>
+            title name
+          </h3>
+          <p className='font-light text-sm inset-1'>
+            a paragraph or context
+          </p>
+        </div>
+        <div>
+          {tags.map((tag) => {
+            <div className={`bg-gray-100 border-1 border-gray-500 px-2 py-1 rounded-md hover:bg-gray-200`}>
+              {tag}
+            </div>
+          })}
+        </div>
     </div>
   )
 }
+
 
 export default UserCard
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import UserCard from '../../Components/Cards/UserCard'
 
 function Home() {
   return (
     <div>
       YO
+      <UserCard/>
     </div>
   )
 }
